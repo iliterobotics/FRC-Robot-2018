dc7497a3c2ea002e583392576838ee31b91458ba
Daniel <dan_pc@live.com>
2015-01-24 16:22:19 -0500

    Took out Mar's rendering code

--- src/org/ilite/vision/camera/opencv/Renderable.java
+++ src/org/ilite/vision/camera/opencv/Renderable.java
@@ -1,6 +1,5 @@
 package org.ilite.vision.camera.opencv;
 
-import java.awt.Color;
 import java.awt.Graphics;
 
 
@@ -9,10 +8,6 @@ public class Renderable implements IRenderable{
 
     @Override
     public void paint(Graphics pG) {
-
-	pG.setColor(Color.MAGENTA);
-	pG.drawLine(0,0, 1000, 1000);
-
 
     } 
 
