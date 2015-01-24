8c9b714e7f188a8ba096534f38cee7f5a086e492
Chris <coconnell84@gmail.com>
2015-01-24 14:51:07 -0500

    Added a drag and drop overlay to deal with issue #7

--- src/org/ilite/vision/camera/opencv/MouseRenderable.java
+++ src/org/ilite/vision/camera/opencv/MouseRenderable.java
@@ -3,6 +3,7 @@ package org.ilite.vision.camera.opencv;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -12,6 +13,8 @@ import javax.swing.JPanel;
 public class MouseRenderable implements IRenderable, MouseListener, MouseMotionListener{
     private final JPanel mView;
     private Point mMousePoint;
+    private Point mDragStart = null;
+    private Point mDragEnd = null;
 
     public MouseRenderable(JPanel pPanel) {
 	mView = pPanel;
@@ -19,13 +22,19 @@ public class MouseRenderable implements IRenderable, MouseListener, MouseMotionL
 
     @Override
     public void mouseDragged(MouseEvent pE) {
-	// TODO Auto-generated method stub
+	System.out.println("DRAGGING");
+	if(mDragStart != null) {
+	    mDragEnd = pE.getPoint();
+	    System.out.println("DRAG END");
+	}
+	mView.repaint();
 
     }
 
     @Override
     public void mouseMoved(MouseEvent pE) {
 	mMousePoint = pE.getPoint();
+
 	mView.repaint();
 
     }
@@ -38,25 +47,28 @@ public class MouseRenderable implements IRenderable, MouseListener, MouseMotionL
 
     @Override
     public void mousePressed(MouseEvent pE) {
-	// TODO Auto-generated method stub
+	mDragStart = pE.getPoint();
+	System.out.println("DRAG STARTED");
+	mDragEnd = mDragStart;
+	mView.repaint();
 
     }
 
     @Override
     public void mouseReleased(MouseEvent pE) {
-	// TODO Auto-generated method stub
+	mDragStart = null;
+	mDragEnd = null;
+	mView.repaint();
 
     }
 
     @Override
     public void mouseEntered(MouseEvent pE) {
-	// TODO Auto-generated method stub
 
     }
 
     @Override
     public void mouseExited(MouseEvent pE) {
-	// TODO Auto-generated method stub
 
     }
 
@@ -69,6 +81,26 @@ public class MouseRenderable implements IRenderable, MouseListener, MouseMotionL
 	    pGraphics.drawLine(0, mMousePoint.y, mView.getWidth(), mMousePoint.y);
 	    pGraphics.drawLine(mMousePoint.x, 0, mMousePoint.x, mView.getHeight());
 	    pGraphics.setColor(oldColor);
+	}
+	
+	if(mDragStart != null && mDragEnd != null) {
+	    System.out.println("DRAW DRAG");
+	   pGraphics.setColor(Color.RED);
+	   
+	   
+	   int startX = Math.min(mDragStart.x, mDragEnd.x);
+	   int startY = Math.min(mDragStart.y, mDragEnd.y);
+	   
+	   int endX = Math.max(mDragStart.x, mDragEnd.x);
+	   int endY = Math.max(mDragStart.y, mDragEnd.y);
+	   
+	   int width = Math.abs(startX -endX);
+	   int height = Math.abs(startY - endY);
+	   
+	   Rectangle aRect = new Rectangle(startX, startY, width, height);
+	   System.out.println(aRect);
+	   
+	   pGraphics.drawRect(startX, startY, width, height);
 	}
 
     }
