2d5368e1abcb59fa7e7142c7ddaa127797760128
Daniel <dan_pc@live.com>
2015-01-31 10:22:25 -0500

    Changed the blob model to work with hsv

--- src/org/ilite/vision/camera/tools/colorblob/BlobModel.java
+++ src/org/ilite/vision/camera/tools/colorblob/BlobModel.java
@@ -6,27 +6,48 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class BlobModel {
     private String name;
-    private float average;
+    private float[] hsv;
+    private static final int HUE = 0;
+    private static final int SATURATION = 1;
+    private static final int VALUE = 2;
     
     public BlobModel() {
-        
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public float getAverage() {
-        return average;
+        hsv = new float[3];
     }
     
     @XmlElement
-    public void setName(String pName) {
-        name = pName;
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @XmlElement
+    public void setAverageHue(float hue) {
+        hsv[HUE] = hue;
+    }
+    
+    @XmlElement
+    public void setAverageValue(float value) {
+        hsv[VALUE] = value;
+    }
+    
+    @XmlElement
+    public void setAverageSaturation(float saturation) {
+        hsv[SATURATION] = saturation;
+    }
+    
+    public float getAverageHue() {
+        return hsv[HUE];
     }
 
-    @XmlElement
-    public void setAverage(float average) {
-        this.average = average;
+    public float getAverageValue() {
+        return hsv[VALUE];
+    }
+
+    public float getAverageSaturation() {
+        return hsv[SATURATION];
+    }
+    
+    public String getName() {
+        return name;
     }
 }
