9c56ae9cd8c664bce074c2746acaa8fb49325e61
Daniel <dan_pc@live.com>
2015-01-31 10:59:31 -0500

    Completed the save dialog class

--- src/org/ilite/vision/camera/opencv/SaveDialog.java
+++ src/org/ilite/vision/camera/opencv/SaveDialog.java
@@ -1,18 +1,22 @@
 package org.ilite.vision.camera.opencv;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.xml.bind.JAXBException;
 
 import org.ilite.vision.camera.tools.colorblob.BlobModel;
+import org.ilite.vision.data.XMLManager;
 
 public class SaveDialog extends JFrame {
     private BufferedImage image;
@@ -22,28 +26,61 @@ public class SaveDialog extends JFrame {
         this.image = image;
         this.model = model;
              
-   
+        JButton saveButton = new JButton("Save");
+        
+        saveButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                
+                int result = fileChooser.showSaveDialog(SaveDialog.this);
+                
+                if(result == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        XMLManager.write(fileChooser.getSelectedFile(), model);
+                    } catch (JAXBException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+            
+        });
+        
+        JButton dropButton = new JButton("Drop");
+        
+        dropButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+            
+        });
+        
+        JPanel buttonPanel = new JPanel();
+       
+        buttonPanel.add(saveButton);
+        buttonPanel.add(dropButton);
+        buttonPanel.setPreferredSize(new Dimension(image.getWidth(), saveButton.getPreferredSize().height + 5));
+        
+        JLabel imageLabel = new JLabel(new ImageIcon(image));
+        
+        JTextField nameTextField = new JTextField("Name");
+        nameTextField.setPreferredSize(new Dimension(image.getWidth(), nameTextField.getPreferredSize().height));
+        
         Box box = Box.createVerticalBox();
-        box.add(new JLabel(new ImageIcon(image)));
-        box.add(new JLabel("Average Color: " + model.averageColor()));
-        box.add(new JTextField("Name"));
+        box.add(imageLabel);
+        box.add(new JLabel("Average Hue: " + model.getAverageHue()));
+        box.add(new JLabel("Average Saturation: " + model.getAverageSaturation()));
+        box.add(new JLabel("Average Value: " + model.getAverageSaturation()));
+        box.add(nameTextField);
+        box.add(buttonPanel);
         
         add(box);
         
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         pack();
         setVisible(true);
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedImage image = ImageIO.read(new File("C:\\Users\\Daniel\\Desktop\\Images\\ground.png"));
-        BlobModel model = new BlobModel();
-        
-        model.setName("Test");
-        model.setBlue(100);
-        model.setGreen(50);
-        model.setRed(75);
-       
-        new SaveDialog(image, model);
     }
 }
