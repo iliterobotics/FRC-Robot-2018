3585f9acd32cc638d936d3d264d5e516abd00d9e
Daniel <dan_pc@live.com>
2015-02-10 20:05:47 -0500

    Image Panel is working

--- src/main/java/org/ilite/vision/camera/opencv/ImagePanel.java
+++ src/main/java/org/ilite/vision/camera/opencv/ImagePanel.java
@@ -1,5 +1,37 @@
 package org.ilite.vision.camera.opencv;
 
-public class ImagePanel {
+import java.awt.image.BufferedImage;
 
+import javax.swing.JPanel;
+
+import org.ilite.vision.api.messages.RobotVisionMsg;
+import org.ilite.vision.api.system.VisionListener;
+import org.ilite.vision.api.system.VisionSystem;
+
+public class ImagePanel implements VisionListener {
+    private BufferedImage currentFrame;
+    private VisionSystem system;
+    private JPanel panel;
+    
+    public ImagePanel() {
+        system = new VisionSystem();
+        
+        system.subscribe(this);
+        
+        panel = new JPanel() {
+            protected void paintComponent(java.awt.Graphics g) {
+                super.paintComponent(g);
+
+                if (currentFrame != null) {
+                    g.drawImage(currentFrame, 0, 0, getWidth(), getHeight(), null);
+                }
+                
+            }
+        };
+    }
+
+    @Override
+    public void onVisionDataRecieved(RobotVisionMsg message) {
+        currentFrame = message.getRawImage();
+    }
 }
