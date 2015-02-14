9d5197e7d5d381b7bf11a87a24229c4f4db82adf
Daniel <dan_pc@live.com>
2015-02-14 09:30:35 -0500

    Overlay image path is now loaded from json

--- src/main/java/org/ilite/vision/data/Configuration.java
+++ src/main/java/org/ilite/vision/data/Configuration.java
@@ -9,19 +9,27 @@ import org.json.JSONException;
 
 public class Configuration {
     private static final String CAMERA_IP;
+    private static final String OVERLAY_IMAGE_PATH;
     
     static {
         String temp = "";
+        String temp1 = "";
         
         try {
             HashMap<String, Object> map = JSONManager.read(new File("properties.json"));
             
-            temp = (String) map.get("CameraIP");       
+            temp = (String) map.get("CameraIP");    
+            temp1 = (String) map.get("OverlayImagePath");
         } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
         
         CAMERA_IP = temp;
+        OVERLAY_IMAGE_PATH = temp1;
+    }
+    
+    public static String getOverlayImagePath() {
+        return OVERLAY_IMAGE_PATH;
     }
     
     public static String getCameraIP() {
