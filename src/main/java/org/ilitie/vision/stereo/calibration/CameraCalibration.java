80251871904637076edb59d211f3c2e6254437cf
Chris <coconnell84@gmail.com>
2016-01-09 14:07:40 -0500

    Updated to allow multiple training images

--- src/main/java/org/ilitie/vision/stereo/calibration/CameraCalibration.java
+++ src/main/java/org/ilitie/vision/stereo/calibration/CameraCalibration.java
@@ -1,6 +1,7 @@
 package org.ilitie.vision.stereo.calibration;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.ilite.vision.camera.opencv.ImageWindow;
@@ -18,79 +19,80 @@ import org.opencv.imgproc.Imgproc;
 
 public class CameraCalibration {
 
-	public static void calibrateCamera(int pBoardWith, int pBoardHeight, Mat pTrainingImage) {
+	public static void calibrateCamera(int pBoardWith, int pBoardHeight, List<Mat> pTrainingImage) {
 		int board_w = pBoardWith;
 		int board_h = pBoardHeight;
 		Size board_sz = new Size(board_w, board_h);
 		//Real location of the cornersin 3D
 		MatOfPoint3f obj = new MatOfPoint3f();
 		MatOfPoint2f corners = new MatOfPoint2f();
+		Size imageSize = null;
 
-		Mat img = pTrainingImage;
-		Mat gray = new Mat();
 		int board_n = board_w*board_h;
-		
 		for (int j=0; j<board_n; j++)
-	    {
+		{
 			obj.push_back(new MatOfPoint3f(new Point3((double)j/(double)board_w, (double)j%(double)board_w, 0.0d)));
-			
-	    }
-		
-		Imgproc.cvtColor(img, gray, Imgproc.COLOR_BGR2GRAY);
 
-		
-		boolean findChessboardCorners = Calib3d.findChessboardCorners(gray, board_sz,corners,Calib3d.CALIB_CB_ADAPTIVE_THRESH | Calib3d.CALIB_CB_FILTER_QUADS);
-		
-		if(findChessboardCorners) {
-			
-			TermCriteria aCriteria = new TermCriteria(TermCriteria.EPS |
-					TermCriteria.MAX_ITER, 30,0.1);
-			Imgproc.cornerSubPix(gray, corners, new Size(11,11), new Size(-1,-1), aCriteria);
-			Calib3d.drawChessboardCorners(gray, board_sz, corners, true);
-			ImageWindow aWindow = new ImageWindow(OpenCVUtils.toBufferedImage(gray));
-			aWindow.show();
-			
-			
-			
+		}
+		List<Mat>allObjects = new ArrayList<Mat>();
 
-			List<Mat>allObjects = new ArrayList<Mat>();
-			allObjects.add(obj);
-			
-			List<Mat>imagePoints = new ArrayList<Mat>();
-			imagePoints.add(corners);
-			
-			calibrateAllImages(img,
-					allObjects, imagePoints);
-		} else {
-			throw new IllegalStateException("Failed to find the chessboard");
+		List<Mat>imagePoints = new ArrayList<Mat>();
+		allObjects.add(obj);
+		for(Mat img : pTrainingImage) {
+			Mat gray = new Mat();
+			imageSize = img.size();
+
+
+			Imgproc.cvtColor(img, gray, Imgproc.COLOR_BGR2GRAY);
+
+
+			boolean findChessboardCorners = Calib3d.findChessboardCorners(gray, board_sz,corners,Calib3d.CALIB_CB_ADAPTIVE_THRESH | Calib3d.CALIB_CB_FILTER_QUADS);
+
+			if(findChessboardCorners) {
+
+				TermCriteria aCriteria = new TermCriteria(TermCriteria.EPS |
+						TermCriteria.MAX_ITER, 30,0.1);
+				Imgproc.cornerSubPix(gray, corners, new Size(11,11), new Size(-1,-1), aCriteria);
+				Calib3d.drawChessboardCorners(gray, board_sz, corners, true);
+				ImageWindow aWindow = new ImageWindow(OpenCVUtils.toBufferedImage(gray));
+				aWindow.show();
+
+
+
+
+				
+
+				imagePoints.add(corners);
+
+			} else {
+				throw new IllegalStateException("Failed to find the chessboard");
+			}
+
+			calibrateAllImages(
+					allObjects, imagePoints,imageSize);
 		}
 
 	}
 
 
-	private static CameraCalibrationData calibrateAllImages(Mat img, 
-			List<Mat> allObjects, List<Mat> imagePoints) {
+	private static CameraCalibrationData calibrateAllImages(
+			List<Mat> allObjects, List<Mat> imagePoints, Size pImageSize) {
 		Mat cameraMatrix = new Mat(3,3,CvType.CV_32FC1);
 		Mat distCoeffs = new Mat();
 		List<Mat> rvecs = new ArrayList<Mat>();
 		List<Mat> tvecs = new ArrayList<Mat>();
-		Calib3d.calibrateCamera(allObjects, imagePoints, img.size(), cameraMatrix, distCoeffs, rvecs, tvecs);
-		Mat imgU = new Mat();
-		Imgproc.undistort(img, imgU, cameraMatrix, distCoeffs);
-		
-		
-		ImageWindow aWindow2 = new ImageWindow(OpenCVUtils.toBufferedImage(imgU));
-		aWindow2.show();
+		Calib3d.calibrateCamera(allObjects, imagePoints, pImageSize, cameraMatrix, distCoeffs, rvecs, tvecs);
+
 		return new CameraCalibrationData(cameraMatrix, distCoeffs);
 	}
 
-	
+
 	public static void main(String[] args) {
 		OpenCVUtils.init();
-		
+
 		Mat chessImage = Highgui.imread("C:/Users/Christopher/gitrepos/FRC-Vision/chessboard2.png");
-		calibrateCamera(9, 6, chessImage);
-		
+		calibrateCamera(9, 6, Collections.singletonList(chessImage));
+
 	}
 
 }
