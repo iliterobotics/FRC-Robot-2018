4945772d688e71e7d6015c69396fed919530edf8
Akasera <atishay.kasera@gmail.com>
2016-02-21 17:52:27 -0500

    Created ValueWindow class to create extra window that displays necessary values to the driver. Changed portion of Configuration class. Added new values to properties.json file

--- src/main/java/com/fauge/robotics/towertracker/ECameraAlignment.java
+++ src/main/java/com/fauge/robotics/towertracker/ECameraAlignment.java
@@ -1,16 +1,22 @@
 package com.fauge.robotics.towertracker;
 
 public enum ECameraAlignment {
-	LEFT("Turn Left"),
-	RIGHT("Turn Right"),
-	CENTER("Centered");
+	LEFT("Turn Left", -1),
+	RIGHT("Turn Right", 1),
+	CENTER("Centered", 0);
 	
 	public String alignment;
+	public int multiplier;
 	
-	private ECameraAlignment(String align){
+	private ECameraAlignment(String align, int multiply){
 		alignment = align;
+		multiplier = multiply;
 	}
 	public String getAlignment(){
 		return alignment;
+	}
+	
+	public int getMultiplier(){
+	    return multiplier;
 	}
 }
--- src/main/java/com/fauge/robotics/towertracker/TowerMessage.java
+++ src/main/java/com/fauge/robotics/towertracker/TowerMessage.java
@@ -9,12 +9,14 @@ public class TowerMessage
     public final double AoE;
     public final String alignment;
     public final BufferedImage bImage;
+    public final int xOffSet;
     
-    public TowerMessage(double distance, double AoE, String alignment, BufferedImage bImage){
+    public TowerMessage(double distance, double AoE, String alignment, BufferedImage bImage, int xOffSet){
         this.distance = distance;
         this.AoE = AoE;
         this.alignment = alignment;
         this.bImage = bImage;
+        this.xOffSet = xOffSet;
         
         
         
