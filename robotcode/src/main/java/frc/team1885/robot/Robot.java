d3089dc613c8ad188f0fbe90443c81de65cf2bbd
Stephen Welch <swordsnclaws@gmail.com>
2017-11-07 21:44:40 -0500

    Add multi-project build.gradle and Eclipse project

---
+++ robotcode/src/main/java/frc/team1885/robot/Robot.java
@@ -0,0 +1,27 @@
+package frc.team1885.robot;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+public class Robot extends SampleRobot {
+    
+	public void robotInit() {
+		
+	}
+	
+	public void disabled() {
+		
+	}
+	
+	public void autonomous() {
+		
+	}
+	
+	public void operatorControl() {
+		
+	}
+	
+	public void test() {
+		
+	}
+	
+}
