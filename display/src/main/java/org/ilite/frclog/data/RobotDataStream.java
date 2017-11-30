1b87f1514fdaa17fcba505c99dae69a558a0dba2
Jesse Knight <flybotix@gmail.com>
2017-11-30 11:49:35 -0500

    Added reset logs mechanism

--- display/src/main/java/org/ilite/frclog/data/RobotDataStream.java
+++ display/src/main/java/org/ilite/frclog/data/RobotDataStream.java
@@ -4,19 +4,17 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.stream.Collectors;
-
-import javax.management.openmbean.OpenMBeanOperationInfoSupport;
 
 import org.ilite.frc.robot.config.SystemSettings;
 import org.ilite.frc.robot.types.EDriveTrain;
@@ -42,6 +40,15 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 public class RobotDataStream {
   private NetworkTable mTable;
   protected static final String NO_DATA = "NO_DATA";
+  private Map<Integer, BufferedWriter> mFilePaths = new HashMap<>();
+  private Map<Integer, List<Codex<?,?>>> mCSVCache = new HashMap<>();
+  private final Timer t = new Timer("File writer");
+  private String mCurrentFolder = "";
+  
+  private static final DateTimeFormatter DATE_FORMAT = 
+    DateTimeFormatter.ofPattern( "uuuuMMMdd-hhmma" )
+      .withLocale( Locale.US )
+      .withZone( ZoneId.systemDefault() );
   
   private final Map<Integer, CodexReceiver<?,?>> mReceivers = new HashMap<>();
   
@@ -67,9 +74,9 @@ public class RobotDataStream {
     }
   }
   
-  
   private RobotDataStream() {
     mTable = NetworkTable.getTable("Generic Config Data");
+    createNewFolder();
     IReceiveProtocol receiver = MessageProtocols.createReceiver(SystemSettings.CODEX_DATA_PROTOCOL, SystemSettings.DRIVER_STATION_CODEX_DATA_RECEIVER_PORT, "");
     registerEnum(EPowerDistPanel.class, receiver);
     registerEnum(ELogitech310.class, receiver);
@@ -77,16 +84,47 @@ public class RobotDataStream {
     registerEnum(ENavX.class, receiver);
     registerEnum(EDriveTrain.class, receiver);
   }
+
+
+  public void resetLogs() {
+    System.out.println("Resetting logs.");
+    createNewFolder();
+    synchronized(mFilePaths) {
+      mFilePaths.values().stream().forEach(writer -> {
+        try {
+          writer.close();
+        } catch (IOException e) {
+          System.err.println(e.getMessage());
+        }
+      });
+      addFileWriter(EPowerDistPanel.class);
+      addFileWriter(ELogitech310.class);
+      addFileWriter(ETalonSRX.class);
+      addFileWriter(ENavX.class);
+      addFileWriter(EDriveTrain.class);
+    }
+  }
   
-  private Map<Integer, BufferedWriter> mFilePaths = new HashMap<>();
-  private Map<Integer, List<Codex<?,?>>> mCSVCache = new HashMap<>();
-  Timer t = new Timer("File writer");
+  private final void createNewFolder() {
+    mCurrentFolder = DATE_FORMAT.format(Instant.now());
+    new File(mCurrentFolder).mkdirs();
+  }
   
   private <V, E extends Enum<E> & CodexOf<V>> void registerEnum( Class<E> pEnum, IReceiveProtocol pReceiver) {
     final int hash = EnumUtils.hashOf(pEnum);
     CodexReceiver<V, E> r = new CodexReceiver<>(pEnum, pReceiver);
     mReceivers.put(hash, r);
     RobotDataElementCache.inst().registerEnum(pEnum);
+    
+    mCSVCache.put(hash, new ArrayList<>());
+    r.addListener(codex -> {
+      synchronized(mCSVCache) {
+        mCSVCache.get(hash).add(codex);
+      }
+    });
+    
+    addFileWriter(pEnum);
+    
     t.scheduleAtFixedRate(new TimerTask() {
       public void run() {
         List<Codex<?,?>> cache = new ArrayList<>();
@@ -96,33 +134,32 @@ public class RobotDataStream {
         }
         if(cache.isEmpty()) {
         } else {
-          BufferedWriter writer = mFilePaths.get(hash);
-          cache.stream().map(codex->codex.toCSV()).forEach(csv -> {
+          // Synch so we can pause writing when a new log file location is chosen
+          synchronized(mFilePaths) {
+            BufferedWriter writer = mFilePaths.get(hash);
+            cache.stream().map(codex->codex.toCSV()).forEach(csv -> {
+              try {
+                writer.write(csv);
+                writer.newLine();
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+            });
             try {
-              writer.write(csv);
-              writer.newLine();
+              writer.flush();
             } catch (IOException e) {
               e.printStackTrace();
             }
-          });
-          try {
-            writer.flush();
-          } catch (IOException e) {
-            e.printStackTrace();
           }
         }
       }
     }, 1000, 1000);
-    
-    mCSVCache.put(hash, new ArrayList<>());
-    r.addListener(codex -> {
-      synchronized(mCSVCache) {
-        mCSVCache.get(hash).add(codex);
-      }
-    });
-    
+  }
+  
+  private <V, E extends Enum<E> & CodexOf<V>> void addFileWriter(Class<E> pEnum) {
+    final int hash = EnumUtils.hashOf(pEnum);
     try {
-      File f = new File(pEnum.getSimpleName() + ".csv");
+      File f = new File(mCurrentFolder, pEnum.getSimpleName() + ".csv");
       String header = null;
       if(!f.exists()) {
         f.createNewFile();
