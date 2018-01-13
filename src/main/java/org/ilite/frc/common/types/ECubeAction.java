e566c020811f1e4559efa08c488ffb473bc5a44e
Nathan <1natechong@gmail.com>
2018-01-13 15:34:15 -0500

    Layed out basic cases with driver preference logic.

--- src/main/java/org/ilite/frc/common/types/ECubeAction.java
+++ src/main/java/org/ilite/frc/common/types/ECubeAction.java
@@ -3,5 +3,6 @@ package org.ilite.frc.common.types;
 public enum ECubeAction {
 	SCALE,
 	SWITCH,
-	EXCHANGE;
+	EXCHANGE,
+	NONE;
 }
--- src/main/java/org/ilite/frc/robot/GetAutonomous.java
+++ src/main/java/org/ilite/frc/robot/GetAutonomous.java
@@ -9,29 +9,123 @@ import openrio.powerup.MatchData;
 import org.ilite.frc.common.types.*;
 
 public class GetAutonomous {
+	private List<Command> commands;
 	private EStartingPosition startingPos;
 	private ECubeAction cubeAction;
 	private ECross crossType;
-	private boolean doComplexAutonomous = false;
+	private boolean doComplexAutonomous;
 
 	public List<Command> getAutonomous(DriveTrain driveTrain) {
-		List<Command> commands = new ArrayList<Command>();
+		commands = new ArrayList<Command>();
 		commands.clear();
-
-		MatchData.OwnedSide switchSide = getSwitchData();
+		
 		MatchData.OwnedSide scaleSide = getScaleData();
-		
-		if(doComplexAutonomous) {
-			
+		MatchData.OwnedSide switchSide = getSwitchData();
+
+		if (doComplexAutonomous) {
+
+			switch (cubeAction) {
+			case SWITCH:
+				doSwitch(scaleSide, switchSide);
+				break;
+			case SCALE:
+				doScale(scaleSide, switchSide);
+				break;
+			case EXCHANGE:
+				doExchange();
+				break;
+			case NONE: 
+				//drive foward?
+				break;
+			}
+
+		} else {
+			// Drive foward
 		}
-		else
-		{
-			//Drive foward (really complex).
-		}
-		
+
 	}
-	
-	
+
+	public void doScale(MatchData.OwnedSide scaleSide, MatchData.OwnedSide switchSide) {
+
+		if (startingPos == EStartingPosition.LEFT && scaleSide == MatchData.OwnedSide.LEFT) {
+			switch (crossType) {
+			case CARPET:
+				break;
+			case PLATFORM:
+				break;
+			case NONE:
+				break;
+			}
+		} else if (startingPos == EStartingPosition.RIGHT && scaleSide == MatchData.OwnedSide.RIGHT) {
+			switch (crossType) {
+			case CARPET:
+				break;
+			case PLATFORM:
+				break;
+			case NONE:
+				break;
+			}
+		} else if (startingPos == EStartingPosition.MIDDLE || switchSide == scaleSide) {
+			switch (crossType) {
+			case CARPET:
+				break;
+			case PLATFORM:
+				break;
+			case NONE:
+				break;
+			}
+		} else {// ignore driver preference.
+				doSwitch(scaleSide, switchSide);
+		}
+	}
+
+	public void doSwitch(MatchData.OwnedSide scaleSide, MatchData.OwnedSide switchSide) {// Switch autonomous routine.
+
+		if (startingPos == EStartingPosition.LEFT && switchSide == MatchData.OwnedSide.LEFT) {
+			switch (crossType) {
+			case CARPET:
+				break;
+			case PLATFORM:
+				break;
+			case NONE:
+				break;
+			}
+		} else if (startingPos == EStartingPosition.RIGHT && switchSide == MatchData.OwnedSide.RIGHT) {
+			switch (crossType) {
+			case CARPET:
+				break;
+			case PLATFORM:
+				break;
+			case NONE:
+				break;
+			}
+		} else if (startingPos == EStartingPosition.MIDDLE || switchSide == scaleSide) {
+			switch (crossType) {
+			case CARPET:
+				break;
+			case PLATFORM:
+				break;
+			case NONE:
+				break;
+			}
+		} else {// ignore driver preference.
+			doScale(scaleSide, switchSide);
+		}
+	}
+
+	public void doExchange() {// Exchange autonomous routine.
+		switch (startingPos) {
+		case LEFT:
+
+			break;
+		case MIDDLE:
+
+			break;
+		case RIGHT:
+
+			break;
+		}
+	}
 
 	public MatchData.OwnedSide getSwitchData() {
 		return MatchData.getOwnedSide(MatchData.GameFeature.SWITCH_NEAR);
