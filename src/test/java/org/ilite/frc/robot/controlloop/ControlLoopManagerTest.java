dea0fabec292756f5925c01602eb4e3849f48d4a
Chris <coconnell84@gmail.com>
2018-01-20 12:35:07 -0500

    Refactored the Mockito imports for readability

--- src/test/java/org/ilite/frc/robot/controlloop/ControlLoopManagerTest.java
+++ src/test/java/org/ilite/frc/robot/controlloop/ControlLoopManagerTest.java
@@ -1,12 +1,18 @@
 package org.ilite.frc.robot.controlloop;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class ControlLoopManagerTest {
 
@@ -16,46 +22,46 @@ public class ControlLoopManagerTest {
 
 	@Before
 	public void setUp() throws Exception {
-		timeProvider = Mockito.mock(ITimerProvider.class);
-		notifer = Mockito.mock(INotifier.class);
+		timeProvider = mock(ITimerProvider.class);
+		notifer = mock(INotifier.class);
 		clm = new ControlLoopManager(notifer,timeProvider);
-		clm.mControlLoops =(List<IControlLoop>) Mockito.mock(List.class);
+		clm.mControlLoops =(List<IControlLoop>) mock(List.class);
 	}
 
 	@Test
 	public void testSetRunningControlLoops_Null() {
 		clm.setRunningControlLoops(( IControlLoop[])null);
-		Mockito.verify(clm.mControlLoops, Mockito.never()).clear();
+		verify(clm.mControlLoops, never()).clear();
 	}
 	
 	@Test
 	public void testSetRunningControlLoops_empty() {
 		clm.setRunningControlLoops(new IControlLoop[0]);
-		Mockito.verify(clm.mControlLoops, Mockito.never()).clear();
-		Mockito.verify(clm.mControlLoops, Mockito.never()).add(Mockito.any(IControlLoop.class));
+		verify(clm.mControlLoops, never()).clear();
+		verify(clm.mControlLoops, never()).add(any(IControlLoop.class));
 	}
 	@Test
 	public void testSetRunningControlLoops_single() {
-		IControlLoop mockedControlLoop = Mockito.mock(IControlLoop.class);
+		IControlLoop mockedControlLoop = mock(IControlLoop.class);
 	
 		clm.setRunningControlLoops(new IControlLoop[] {mockedControlLoop});
 		
-		Mockito.verify(clm.mControlLoops, Mockito.times(1)).clear();
-		Mockito.verify(clm.mControlLoops, Mockito.times(1)).add(mockedControlLoop);
+		verify(clm.mControlLoops, times(1)).clear();
+		verify(clm.mControlLoops, times(1)).add(mockedControlLoop);
 	}
 
 	@Test
 	public void testStart_NotRunning() {
 		clm.mControlLoops = new ArrayList<>();
-		IControlLoop mock = Mockito.mock(IControlLoop.class);
+		IControlLoop mock = mock(IControlLoop.class);
 		clm.mControlLoops.add(mock);
-		Mockito.when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
+		when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
 		
 		Assert.assertFalse(clm.mIsRunning);
 
 		clm.start();
 		
-		Mockito.verify(mock).initialize(0.5d);
+		verify(mock).initialize(0.5d);
 		Assert.assertTrue(clm.mIsRunning);
 		
 	}
@@ -63,15 +69,15 @@ public class ControlLoopManagerTest {
 	public void testStart_Running() {
 		clm.mIsRunning = true;
 		clm.mControlLoops = new ArrayList<>();
-		IControlLoop mock = Mockito.mock(IControlLoop.class);
+		IControlLoop mock = mock(IControlLoop.class);
 		clm.mControlLoops.add(mock);
-		Mockito.when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
+		when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
 		
 		Assert.assertTrue(clm.mIsRunning);
 
 		clm.start();
 		
-		Mockito.verify(mock, Mockito.never()).initialize(0.5d);
+		verify(mock, never()).initialize(0.5d);
 		Assert.assertTrue(clm.mIsRunning);
 		
 	}
@@ -79,62 +85,62 @@ public class ControlLoopManagerTest {
 	@Test
 	public void testStop_isRunning() {
 		clm.mIsRunning = true;
-		Mockito.when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
+		when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
 		
-		IControlLoop mockedConrolLoop = Mockito.mock(IControlLoop.class);
+		IControlLoop mockedConrolLoop = mock(IControlLoop.class);
 		clm.mControlLoops = new ArrayList<>();
 		clm.mControlLoops.add(mockedConrolLoop);
 
 		
 		clm.stop();
 		
-		Mockito.verify(mockedConrolLoop, Mockito.times(1)).shutdown(0.5d);
-		Mockito.verify(notifer, Mockito.times(1)).stop();
+		verify(mockedConrolLoop, times(1)).shutdown(0.5d);
+		verify(notifer, times(1)).stop();
 		
 	}
 	
 	@Test
 	public void testStop_isNotRunning() {
 		clm.mIsRunning = false;
-		Mockito.when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
+		when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
 		
-		IControlLoop mockedConrolLoop = Mockito.mock(IControlLoop.class);
+		IControlLoop mockedConrolLoop = mock(IControlLoop.class);
 		clm.mControlLoops = new ArrayList<>();
 		clm.mControlLoops.add(mockedConrolLoop);
 
 		
 		clm.stop();
 		
-		Mockito.verify(mockedConrolLoop, Mockito.never()).shutdown(0.5d);
-		Mockito.verify(notifer, Mockito.never()).stop();
+		verify(mockedConrolLoop, never()).shutdown(0.5d);
+		verify(notifer, never()).stop();
 		
 	}
 
 	@Test
 	public void testRun_isRunning() {
 		clm.mIsRunning = true;
-		Mockito.when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
-		IControlLoop mockedControlLoop = Mockito.mock(IControlLoop.class);
+		when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
+		IControlLoop mockedControlLoop = mock(IControlLoop.class);
 		clm.mControlLoops = new ArrayList<>();
 		clm.mControlLoops.add(mockedControlLoop);
 		
 		clm.run();
 		
-		Mockito.verify(mockedControlLoop, Mockito.times(1)).loop(0.5d);
+		verify(mockedControlLoop, times(1)).loop(0.5d);
 		
 	}
 	
 	@Test
 	public void testRun_isNotRunning() {
 		clm.mIsRunning = false;
-		Mockito.when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
-		IControlLoop mockedControlLoop = Mockito.mock(IControlLoop.class);
+		when(timeProvider.getFPGATimestamp()).thenReturn(0.5d);
+		IControlLoop mockedControlLoop = mock(IControlLoop.class);
 		clm.mControlLoops = new ArrayList<>();
 		clm.mControlLoops.add(mockedControlLoop);
 		
 		clm.run();
 		
-		Mockito.verify(mockedControlLoop, Mockito.never()).loop(0.5d);
+		verify(mockedControlLoop, never()).loop(0.5d);
 		
 	}
 
