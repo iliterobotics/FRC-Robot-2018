d7cfba4a3df9e01902aa0a49dc8cf4fc1b1a5a13
SilentWanderer <swordsnclaws@verizon.net>
2018-02-18 14:23:34 -0500

    Add Carriage to Master

--- src/main/java/org/ilite/frc/robot/modules/Carriage.java
+++ src/main/java/org/ilite/frc/robot/modules/Carriage.java
@@ -2,88 +2,143 @@ package org.ilite.frc.robot.modules;
 
 import org.ilite.frc.common.types.ELogitech310;
 import org.ilite.frc.robot.Data;
+
+import com.flybotix.hfr.util.log.ILog;
+import com.flybotix.hfr.util.log.Logger;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
 
 public class Carriage implements IModule{
 
-	private Solenoid solenoid1, solenoid2, solenoid3;
-	private double kickTimer;
-	private final int kickDelay = 1;
-	private final int releaseDelay = 5;
-	private double releaseTimer;
-	private double currentTime;
-	private Data mData;
-	private boolean isScheduled;
-	private boolean isPushing;
-	
-	
-	public Carriage(Data pData)
-	{
-		mData = pData;
-		isScheduled = false;
-		solenoid1 = new Solenoid(11, 1);
-		solenoid2 = new Solenoid(11, 2);
-		solenoid3 = new Solenoid(11, 3);
-	}
-	
-	@Override
-	public void shutdown(double pNow) {
-		
-		
-	}
-	@Override
-	public void initialize(double pNow) {
-		solenoid1.set(false);
-		solenoid2.set(false);
-		solenoid3.set(false);
-		
-	}
-	@Override
-	public boolean update(double pNow)
-	{
-		if(mData.driverinput.isSet(ELogitech310.DPAD_LEFT) || isScheduled)
-		{
-			schedule(pNow);
-		}
-		else
-		{
-			reset();
-		}
-		currentTime = pNow;
-		return false;
-	}	
-		
-	public void schedule(double pNow)
-	{
-		if(!isScheduled)
-		{
-			kickTimer = pNow + kickDelay;
-			releaseTimer = pNow + releaseDelay;
-			isScheduled = true;
-		}
-		else
-		{
-			if(currentTime >= kickTimer)
-			{
-				//kick
-				//testing code:
-				solenoid1.set(true);
-			}
-			if(currentTime >= releaseTimer)
-			{
-				//release
-				solenoid2.set(true);
-				solenoid3.set(true);
-				reset();
-			}
-		}
-	}
-	public void reset()
-	{
-		solenoid1.set(false);
-		solenoid2.set(false);
-		solenoid3.set(false);
-		//undo kick and release
-	}
+  private Solenoid solenoidGrab, solenoidPop;
+  private double kickTimer;
+  private static final int KICK_DELAY = 1;
+  private static final int RELEASE_DELAY = 5;
+  private double releaseTimer;
+  private double currentTime;
+  private Data mData;
+  private boolean isScheduled;
+  private static DigitalInput beamBreak;
+  private carriageState currentState;
+  private double kickStartTime;
+  
+  private static final ILog log = Logger.createLog(Carriage.class);
+    
+  
+  
+  public Carriage(Data pData, DigitalInput beamBreak)
+  {
+    Carriage.beamBreak = beamBreak;
+    mData = pData;
+    isScheduled = false;
+    solenoidGrab = new Solenoid(11, 2);
+    solenoidPop = new Solenoid(11, 3);
+    setNoCube();
+    currentState = carriageState.CUBE;
+  }
+  
+  public enum carriageState
+  {
+    NOCUBE,
+    CUBE,
+    KICKING;
+  }
+  
+  @Override
+  public void shutdown(double pNow) {
+    
+    
+  }
+  @Override
+  public void initialize(double pNow) {
+    setHaveCube();
+  }
+  @Override
+  public boolean update(double pNow)
+  {
+    log.debug(currentState.toString());
+    currentTime = pNow;
+    
+    switch(currentState)
+    {
+    case CUBE:
+    setHaveCube();
+    if(mData.driverinput.isSet(ELogitech310.DPAD_LEFT) || isScheduled)
+    {
+      kickStartTime = pNow;
+      currentState = carriageState.KICKING;
+    }
+    break;
+    
+    case KICKING:
+      isKicking(pNow);
+    break;
+    
+    case NOCUBE:
+      setNoCube();
+      if(getBeamBreak())
+      {
+        currentState = carriageState.CUBE;
+      }
+    }
+    
+    return false;
+  } 
+    
+  public void isKicking(double pNow)
+  {
+    if(!isScheduled)
+    {
+      kickTimer = pNow + KICK_DELAY;
+      releaseTimer = pNow + RELEASE_DELAY;
+      isScheduled = true;
+    }
+    else
+    {
+      log.debug("CurrTime: " + currentTime + " kickStartTime= " + kickStartTime + " kickTimer= " + kickTimer + " releaseTime= " + releaseTimer);
+      if((currentTime - kickStartTime) >= kickTimer)
+      {
+        //kick
+      }
+      if((currentTime - kickStartTime) >= releaseTimer)
+      {
+        //release
+        setNoCube();
+        reset();
+        currentState = carriageState.NOCUBE;
+        kickStartTime = 0;
+      }
+    }
+  }
+  public void reset()
+  {
+    setNoCube();
+    //undo kick and release
+  }
+  
+  public boolean getBeamBreak()
+  {
+    boolean returnVal = false;
+    
+    if(beamBreak != null) {
+    returnVal = beamBreak.get(); 
+    }
+    
+    return returnVal;
+    
+  }
+  
+  private void setHaveCube()
+  {
+    solenoidGrab.set(true);
+    solenoidPop.set(false);
+  }
+  
+  private void setNoCube()
+  {
+    solenoidGrab.set(false);
+    solenoidPop.set(true);
+  }
 	
 }
