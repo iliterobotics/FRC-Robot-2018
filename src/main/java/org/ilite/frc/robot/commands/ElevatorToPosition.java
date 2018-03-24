dffff575699c65137f52f646800b80533432ec2e
George <gealphonse22@gmail.com>
2018-03-24 09:13:30 -0400

    ElevatorToPosition

--- src/main/java/org/ilite/frc/robot/commands/ElevatorToPosition.java
+++ src/main/java/org/ilite/frc/robot/commands/ElevatorToPosition.java
@@ -26,7 +26,7 @@ public class ElevatorToPosition implements ICommand {
   @Override
   public boolean update(double pNow) {
     mElevator.setPosition(mPosition);
-    if(mElevator.getCurrentEncoderTicks() == mPosition.encoderThreshold) return true;
+    if(mPosition.inRange(mElevator.getCurrentEncoderTicks())) return true;
     if(pNow - mStartTime >= mTimeout) return true;
     return false;
   }
