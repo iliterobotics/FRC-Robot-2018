b7d4a7abe59c4daf9aeaef21201c51231f4c18a8
Stephen Welch <stephenwelchva@gmail.com>
2018-10-16 23:39:45 -0400

    Add FollowTrajectory command

---
+++ src/main/java/org/ilite/frc/robot/commands/FollowTrajectory.java
@@ -0,0 +1,49 @@
+package org.ilite.frc.robot.commands;
+
+import com.team254.lib.util.ReflectingCSVWriter;
+import control.DriveMotionPlanner;
+import lib.geometry.Pose2d;
+import lib.geometry.Pose2dWithCurvature;
+import lib.trajectory.Trajectory;
+import lib.trajectory.timing.TimedState;
+import org.ilite.frc.robot.modules.drivetrain.TrajectoryFollower;
+
+public class FollowTrajectory implements ICommand {
+
+    private Trajectory<TimedState<Pose2dWithCurvature>> mTrajectory;
+    private TrajectoryFollower mTrajectoryFollower;
+    private boolean mResetPose;
+
+    private boolean mWriteToCsv = false;
+
+    public FollowTrajectory(Trajectory<TimedState<Pose2dWithCurvature>> pTrajectory, TrajectoryFollower pTrajectoryFollower, boolean pResetPose) {
+        mTrajectory = pTrajectory;
+        mTrajectoryFollower = pTrajectoryFollower;
+        mResetPose = pResetPose;
+    }
+
+    @Override
+    public void initialize(double pNow) {
+        mTrajectoryFollower.getDriveController().setTrajectory(mTrajectory, mResetPose);
+    }
+
+    @Override
+    public boolean update(double pNow) {
+        if(mTrajectoryFollower.getDriveController().isDone()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void shutdown(double pNow) {
+
+    }
+
+    public FollowTrajectory setWriteToCsv() {
+        mWriteToCsv = true;
+        return this;
+    }
+
+}
