e34b94cdcf925c665673fff7e0f532265609bda9
Stephen Welch <stephenwelchva@gmail.com>
2018-10-30 23:12:26 -0400

    Add script to automatically retrieve and delete logs on RIO

--- retrieve-logs.sh
+++ retrieve-logs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+echo Removing locally stored logs...
+rm *.csv
+
+echo Retrieving most recent logs from robot...
 scp lvuser@10.18.85.2:ODOMETRY.csv .
 scp lvuser@10.18.85.2:TRAJECTORY.csv .
 scp lvuser@10.18.85.2:SETPOINT.csv .
-ssh lvuser@10.18.85.2 "rm ODOMETRY.csv"
-ssh lvuser@10.18.85.2 "rm TRAJECTORY.csv"
-ssh lvuser@10.18.85.2 "rm SETPOINT.csv"
-ssh lvuser@10.18.85.2 "rm VELOCITY_DATA.csv"
-ssh lvuser@10.18.85.2 "rm ACCEL_DATA.csv"
+
+echo Cleaning up log files on robot...
+ssh lvuser@10.18.85.2 "rm *.csv"
