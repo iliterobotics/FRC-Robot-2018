fb0c1929e97d8edfcf9500197d005412a22d4d34
Stephen Welch <stephenwelchva@gmail.com>
2018-11-19 00:01:49 -0500

    Re-characterize drivebase

--- src/main/java/org/ilite/frc/robot/modules/drivetrain/MikeyProfile.java
+++ src/main/java/org/ilite/frc/robot/modules/drivetrain/MikeyProfile.java
@@ -7,32 +7,32 @@ import profiles.RobotProfile;
 public class MikeyProfile implements RobotProfile {
     @Override
     public double getLeftVoltPerAccel() {
-        return 0.037288968942691854;
+        return 0.03217677393640587 * 1.5;
     }
 
     @Override
     public double getLeftVoltPerSpeed() {
-        return 0.21681437087695135 * 0.80;
+        return 0.2145841808650226;
     }
 
     @Override
     public double getLeftFrictionVoltage() {
-        return 0.9050766127768425;
+        return 0.7892487640968108;
     }
 
     @Override
     public double getRightVoltPerAccel() {
-        return 0.037288968942691854;
+        return 0.02757180183116733 * 1.5;
     }
 
     @Override
     public double getRightVoltPerSpeed() {
-        return 0.2168143708769513 * 0.80;
+        return 0.20865527274973875;
     }
 
     @Override
     public double getRightFrictionVoltage() {
-        return 0.9050766127768425;
+        return 0.8438511952245853;
     }
 
     @Override
@@ -52,7 +52,7 @@ public class MikeyProfile implements RobotProfile {
 
     @Override
     public double getLinearInertia() {
-        return 1.0;
+        return 60.0;
     }
 
     @Override
