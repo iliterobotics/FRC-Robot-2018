0088af2149d481303d6988edee8f41944bb00757
Stephen Welch <stephenwelchva@gmail.com>
2018-11-20 03:51:51 -0500

    Store retrieved logs in their own folder

--- retrieve-logs.sh
+++ retrieve-logs.sh
@@ -4,9 +4,9 @@ echo Removing locally stored logs...
 rm *.csv
 
 echo Retrieving most recent logs from robot...
-scp lvuser@10.18.85.2:ODOMETRY.csv .
-scp lvuser@10.18.85.2:TRAJECTORY.csv .
-scp lvuser@10.18.85.2:SETPOINT.csv .
+scp lvuser@10.18.85.2:logs/ODOMETRY.csv .
+scp lvuser@10.18.85.2:logs/TRAJECTORY.csv .
+scp lvuser@10.18.85.2:logs/SETPOINT.csv .
 
 echo Cleaning up log files on robot...
 ssh lvuser@10.18.85.2 "rm *.csv"
