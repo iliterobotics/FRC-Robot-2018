17709cb2af8324d9a59767c4f61efafff2b59578
Stephen Welch <stephenwelchva@gmail.com>
2018-11-23 01:00:17 -0500

    Retrieve all logs ending in .csv

--- retrieve-logs.sh
+++ retrieve-logs.sh
@@ -4,9 +4,7 @@ echo Removing locally stored logs...
 rm *.csv
 
 echo Retrieving most recent logs from robot...
-scp lvuser@10.18.85.2:logs/ODOMETRY.csv .
-scp lvuser@10.18.85.2:logs/TRAJECTORY.csv .
-scp lvuser@10.18.85.2:logs/SETPOINT.csv .
+scp lvuser@10.18.85.2:*.csv .
 
 echo Cleaning up log files on robot...
 ssh lvuser@10.18.85.2 "rm *.csv"
