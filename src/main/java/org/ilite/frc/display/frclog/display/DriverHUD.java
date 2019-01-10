893a8e821babf593104a43adf3fc8a6834a84f51
Daniel <danielchristopher19@gmail.com>
2019-01-10 17:12:30 -0500

    temporary commit

--- src/main/java/org/ilite/frc/display/frclog/display/DriverHUD.java
+++ src/main/java/org/ilite/frc/display/frclog/display/DriverHUD.java
@@ -18,6 +18,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.paint.CycleMethod;
 
 import org.ilite.frc.common.config.SystemSettings;
 import org.ilite.frc.common.types.ELogitech310;
@@ -30,48 +33,72 @@ import java.util.Set;
 
 public class DriverHUD extends Application {
 
-    Image abtn = new Image( "abtn.png" );
-    Image bbtn = new Image( "bbtn.png" );
-    Image wht = new Image( "White.png" );
-    Image xbtn = new Image( "xbtn.png" );
-    Image ybtn = new Image( "ybtn.png" );
+    // KeyButtons aKey = new KeyButtons( abtn, "A" );
+    // KeyButtons bKey = new KeyButtons( bbtn, "B" );
+    // KeyButtons xKey = new KeyButtons( xbtn, "X" );
+    // KeyButtons yKey = new KeyButtons( ybtn, "Y" );
 
+    private String state = "";
+    private String realState = "";
 
-    KeyButtons aKey = new KeyButtons( abtn, "A" );
-    KeyButtons bKey = new KeyButtons( bbtn, "B" );
-    KeyButtons xKey = new KeyButtons( xbtn, "X" );
-    KeyButtons yKey = new KeyButtons( ybtn, "Y" );
+    public void start(Stage stage) {
 
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, Color.PURPLE);
+        Canvas canvas = new Canvas(1000, 300);
 
-    public void start( Stage stage ) {
+        // Initialize images
 
-        BorderPane root = new BorderPane( );
-        Scene scene = new Scene( root, Color.WHITE );
-        Canvas canvas = new Canvas( 500, 500 );
+        //Gradients
+        Stops[] stops = new Stop[] { new Stop( 0, Color.PURPLE ), new Stop( 1, Color.GREEN ) };
+        LinearGradient linear = new LinearGradient( 0, 0, 1, 0, true, CycleMethod.NO_CYCLE, stops );
 
-        //Initialize images
+        Rectangle rect = new Rectangle( 0, 0, 1000, 300);
+        rect.setFill( linear );
+        root.getChildren().add( rect );
 
-        //Draw Images
+        // Draw Images
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.drawImage( abtn, canvas.getWidth()/2, canvas.getHeight()/2 );
+        // gc.drawImage( abtn, canvas.getWidth()/2, canvas.getHeight()/2 );
 
-        root.setCenter( canvas );
+        root.setCenter(canvas);
 
-        stage.setScene( scene );
+        stage.setScene(scene);
 
-        HashSet<String> inputs = new HashSet<>(  );
-        ArrayList<KeyButtons> registered = new ArrayList<>(  );
+        HashSet<String> inputs = new HashSet<>();
+        ArrayList<KeyButtons> registered = new ArrayList<>();
 
         // scene.setOnKeyPressed( e -> inputs.add( e.getCode().toString() ));
 
         // scene.setOnKeyReleased( e -> inputs.remove( e.getCode().toString() ) );
 
-        registered.add( yKey );
+        // registered.add( yKey );
 
         final long startTime = System.nanoTime();
 
         new AnimationTimer() {
-            public void handle( long currentNanoTime ) {
+
+            public void handle(long currentNanoTime) {
+                // state = SystemSettings.LOGGING_TABLE.getInstance().getEntry("Carriage
+                // State").getString("heck.");
+
+                // System.out.println(SystemSettings.LOGGING_TABLE.getInstance().getEntry("Carriage State").getString("heck."));
+
+                if (!(realState.equals(state))) {
+                    realState = state;
+                }
+
+                switch (realState) {
+                case ("KICKING"):
+                    System.out.println("KICKING");
+                case ("RESET"):
+                    System.out.println("RESET");
+                case ("GRAB_CUBE"):
+                    System.out.println("GRAB_CUBE");
+                default:
+                    System.out.println(state);
+
+                }
 
             }
         }.start();
@@ -81,9 +108,10 @@ public class DriverHUD extends Application {
     }
 
     public static void main(String[] args) {
-      //TODO: Make more "professional"
-        while(true)System.out.println( SystemSettings.LOGGING_TABLE.getInstance().getEntry("Carriage State").getString("heck.") );
-        // launch( args );
+        // TODO: Make more "professional"
+        // while (true)System.out.println(SystemSettings.LOGGING_TABLE.getInstance().getEntry("Carriage State").getString("heck."));
+        // C:\Users\Daniel T\Desktop\robotics\FRC-Robot-2018\lib\jfxrt.jar
+        launch(args);
     }
 
 }
